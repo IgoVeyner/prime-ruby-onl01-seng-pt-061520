4b90d5a178fd99d068e61eca6641dde4d28dba69
IgoVeyner <igorveyner95@gmail.com>
2020-06-18 14:55:02 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -4,9 +4,13 @@ def prime?(int)
     if int == 0 || int == 1 || int.negative? == true
         return false
     end
-    factors = []
-    factors = (2..(int - 1)).to_a
-    factors.none? do |n|
+
+    # My Original Answer, I need to get better at simplifiying / shortening code
+    # factors = []
+    # factors = (2..(int - 1)).to_a
+    # factors.none? do |n|
+
+    (2..(int - 1)).to_a.none? do |n|
         int % n == 0
     end
 end 
