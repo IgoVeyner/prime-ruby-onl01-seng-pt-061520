041c0a103b85922cfa90df45d33d4d6c0dcbcd54
IgoVeyner <igorveyner95@gmail.com>
2020-06-18 14:20:49 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
+require 'pry'
 # Add  code here!
+def prime?(int)
+    if int == 0 || int == 1 || int.negative? == true
+        return false
+    end
+    factors = []
+    factors = (2..(int - 1)).to_a
+    factors.none? do |n|
+        int % n == 0
+    end
+end 
